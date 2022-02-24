fab4fed8d267c958cc28bb26282016aa77be69bb
campbell mercer <campbell.d.m.b@gmail.com>
2022-02-24 21:39:35 +1100

    Basic layout and themeing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.scss";
+import "../styles/variables.scss";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>CampbellMBXJ | Portfolio</title>
+        <meta
+          name="description"
+          content="The software portfolio of Campbell Mercer-Butcher"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
